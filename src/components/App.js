dd1a72073a5ab81a4e8d596b14af3b74d96848ef
Mercedes <mermfb@gmail.com>
2021-02-18 16:10:45 +0100

    Add design component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import "../stylesheets/App.scss";
 import logoHeader from "../images/thisismeLogo@2x.png";
 import logoCard from "../images/logo-card.png";
+import Design from "./Design";
 
 function App() {
   /* MARTA */
@@ -88,84 +89,7 @@ function App() {
         <form className="form js-form" action="" method="POST">
           <div className="form__wrapper">
             {/* MERCHE */}
-            <fieldset className="collapsable js-slide1">
-              <legend className="collapsable__header js-design">
-                <i
-                  className="collapsable__header--icon fa fa-object-ungroup"
-                  aria-hidden="true"
-                ></i>
-                <span className="collapsable__title">Diseña</span>
-                <i
-                  className="collapsable__header--arrow  collapsable__turn fa fa-angle-up"
-                  aria-hidden="true"
-                ></i>
-              </legend>
-
-              <div className="open collapsable__hide  design  js-content">
-                <label htmlFOR="palette1" className="design__label">
-                  <div className="colors">Colores</div>
-                  <input
-                    id="palette1"
-                    type="radio"
-                    value="1"
-                    name="palette"
-                    className="design__radio js-palette"
-                    checked
-                  />
-
-                  <ul className="palette1">
-                    <li className="design__color color1">Color A</li>
-                    <li className="design__color color2">Color B</li>
-                    <li className="design__color color3">Color C</li>
-                  </ul>
-                </label>
-
-                <label htmlFOR="palette2" className="design__label">
-                  <input
-                    id="pallete2"
-                    type="radio"
-                    value="2"
-                    name="palette"
-                    className="design__radio js-palette"
-                  />
-                  <ul className="palette2">
-                    <li className="design__color color1">Color A</li>
-                    <li className="design__color color2">Color B</li>
-                    <li className="design__color color3">Color C</li>
-                  </ul>
-                </label>
-
-                <label htmlFOR="palette3" className="design__label">
-                  <input
-                    id="palette3"
-                    type="radio"
-                    value="3"
-                    name="palette"
-                    className="design__radio js-palette"
-                  />
-                  <ul className="palette3">
-                    <li className="design__color color1">Color A</li>
-                    <li className="design__color color2">Color B</li>
-                    <li className="design__color color3">Color C</li>
-                  </ul>
-                </label>
-
-                <label htmlFOR="palette4" className="design__label">
-                  <input
-                    id="palette4"
-                    type="radio"
-                    value="4"
-                    name="palette"
-                    className="design__radio js-palette"
-                  />
-                  <ul className="palette4">
-                    <li className="design__color color1">Color A</li>
-                    <li className="design__color color2">Color B</li>
-                    <li className="design__color color3">Color C</li>
-                  </ul>
-                </label>
-              </div>
-            </fieldset>
+            <Design />
             {/* NOBODY */}
             <fieldset className="collapsable js-slide2">
               <legend className="collapsable__header js-fill">
