54d02ef507cbb34bd96b81315d5fb62cc12880f7
CCrisBIB <cristinabiglesias@gmail.com>
2021-02-22 12:48:41 +0100

    Creat new component: ShareCreate.js

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,4 +1,5 @@
 import "../stylesheets/layout/_shareSection.scss";
+import ShareCreated from "./ShareCreated";
 
 function Share(props) {
   return (
@@ -20,24 +21,7 @@ function Share(props) {
           </section>
         </div>
       </fieldset>
-      <section className="created js-created created--hidden">
-        <h3 className="created__title">La tarjeta ha sido creada:</h3>
-        <a className="created__url js-card-result" href=""></a>
-        <button className="created__btnTwitter">
-          <a
-            href="https://twitter.com/?lang=es"
-            target="_blank"
-            className="twitter"
-          >
-            <i className="fa fa-twitter" aria-hidden="true"></i> Compartir en
-            twitter
-          </a>
-        </button>
-      </section>
-      <section className="js-error created__error created--hidden">
-        <h4 className="created__error--title">Por favor complete los datos:</h4>
-        <p className="js-consoleError"></p>
-      </section>
+      <ShareCreated />
     </>
   );
 }
