a3b865d2c3ebaea6d5472f38509a6e2184014a6f
Marta Cano <martanchor@gmail.com>
2021-02-25 13:01:18 +0100

    start lifting inphotocard and input

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import PhotoCard from "./PhotoCard";
 import Form from "./Form";
 
 function Main() {
+  const [name, setName]= useState('pepiner');
   return (
     <main className="mainProfile mainProfile__wrapper">
       {/* <div className="mainProfile__wrapper"> */}
