1436b32683dc9ee12441cab0452bb85078e37952
Marta Cano <martanchor@gmail.com>
2021-03-17 11:11:17 +0100

    update package.json and api

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -14,30 +14,36 @@
 
 // export default senddatatoapi;
 
+const isDevEnviroment = process.env.NODE_ENV === "development"; // Booleano
+const apiUrl = isDevEnviroment ?
+    "http://localhost:3000" :
+    "https://delicious-profile-card.herokuapp.com/#/";
+
 const getCard = () => {
-  return (
-    fetch("/card/:id:/")
-      //return fetch("http://localhost:3001/card/card")
-      .then((response) => response.json())
-      .then((data) => {
-        return data;
-      })
-  );
+    return (
+        fetch(`${apiUrl}/card/:id:/`)
+        //return fetch("http://localhost:3001/card/card")
+
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        })
+    );
 };
 
 const sendCard = (data) => {
-  return fetch("/card", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
+    return fetch("/card", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        });
 };
 
 export default {
-  getCard: getCard,
-  sendCard: sendCard,
+    getCard: getCard,
+    sendCard: sendCard,
 };
